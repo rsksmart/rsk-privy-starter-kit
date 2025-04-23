349d902f526ad7bf703579e976f38fe3e6889599
Juan Agudelo <juan.agudelo@rootstocklabs.com>
2025-04-23 05:48:59 +0300

    chore: privy + wagmi config

--- src/config/providers.tsx
+++ src/config/providers.tsx
@@ -1,23 +1,52 @@
-import React from "react";
-import { WagmiProvider } from "wagmi";
-import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./wagmiProviderConfig";
-import { rainbowkitConfig } from "./rainbowkitConfig";
+'use client';
 
-const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <WagmiProvider config={rainbowkitConfig}>
-    <QueryClientProvider client={queryClient}>
-      <RainbowKitProvider
-        theme={darkTheme({
-          accentColor: "#f28a0e",
-          accentColorForeground: "black",
-        })}
-      >
-        {children}
-      </RainbowKitProvider>
-    </QueryClientProvider>
-  </WagmiProvider>
-);
+import {  QueryClientProvider } from '@tanstack/react-query';
+import { http } from 'viem';
+import { rootstock, rootstockTestnet } from 'viem/chains';
+import type { PrivyClientConfig } from '@privy-io/react-auth';
+import { PrivyProvider } from '@privy-io/react-auth';
+import { WagmiProvider, createConfig } from '@privy-io/wagmi';
+import { queryClient } from './wagmiProviderConfig';
 
-export default Providers;
+
+
+export const wagmiConfig = createConfig({
+  chains: [rootstock, rootstockTestnet],
+  transports: {
+    [rootstock.id]: http(),
+    [rootstockTestnet.id]: http(),
+  },
+});
+
+const privyConfig: PrivyClientConfig = {
+  embeddedWallets: {
+    createOnLogin: 'users-without-wallets',
+  },
+  loginMethods: ['wallet', 'email', 'sms', 'google', 'apple'],
+  appearance: {
+    showWalletLoginFirst: false,
+    theme: 'dark',
+    loginMessage: 'Please sign this message to confirm your identity',
+    walletChainType: 'ethereum-only',
+  },
+  defaultChain: rootstockTestnet,
+  supportedChains: [rootstock, rootstockTestnet],
+};
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrivyProvider
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      appId="cm9djsotz028nih0n0y6gsreb"
+      clientId="client-WY5itZyfwpHNt1terGEwasCdPZaToUAQnX73GdNPA1WxX"
+      config={privyConfig}
+    >
+      <QueryClientProvider client={queryClient}>
+        <WagmiProvider config={wagmiConfig} reconnectOnMount={false} >
+          {children}
+        </WagmiProvider>
+      </QueryClientProvider>
+    </PrivyProvider>
+  );
+}
